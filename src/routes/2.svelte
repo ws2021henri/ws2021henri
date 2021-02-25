c7a43c28d1537518811830ff60cccca49fee9909
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2021-02-25 10:22:52 +0100

    Edité

--- src/routes/2.svelte
+++ src/routes/2.svelte
@@ -1,3 +1,14 @@
+<script>
+  let visible = [true];
+
+  function hotkeys(e) {
+    let key = e.key;
+    if (key === "A" || key === "a") visible[0] = !visible[0];
+  }
+</script>
+
+<svelte:window on:keydown|preventDefault={hotkeys} />
+
 <svelte:head>
   <title>All at once</title>
 </svelte:head>
@@ -5,9 +16,9 @@
 <div class="container">
   <div class="content">
     <h2
-      >HENRI vu comme un prototype implique de travailler en même temps :<br
+      >HENRI envisagé comme prototype signifie travailler en même temps :<br
       /><span class="en"
-        >HENRI as a prototype involves working all at once on:</span
+        >HENRI seen as a prototype involves working all at once on:</span
       ></h2
     >
     <ul>
@@ -16,18 +27,27 @@
           >Visual design and user experience</span
         >
       </li>
-      <li style="margin-left: 1rem;"
+      <li class="separate" style="margin-left: 1rem;"
         >Architecture des données / <span class="en">Data architecture</span>
-      </li><li style="margin-left: 2rem;"
+      </li>
+
+      <li style="margin-left: 2rem;"
         >Développement technique / <span class="en">Technical development</span
         ></li
       >
+      <hr class:visible={visible[0]} />
       <li style="margin-left: 3rem;"
         >Édition de contenu (données, textes, images) / <span class="en"
           >Content editing (data, texts and images)</span
         ></li
       >
     </ul>
+
+    <p style="font-size: 1.5rem; margin-top: 48px; margin-left: 3rem;"
+      >... même après le lancement de la plate-forme.<br /><span class="en"
+        >... even after the platform has been launched.</span
+      ></p
+    >
   </div>
 </div>
 
@@ -52,6 +72,28 @@
   ul {
     list-style-type: none;
     padding: 0;
+  }
+
+  hr {
+    display: none;
+    padding: 0;
+    margin: 12px;
+    border: solid 4px #fff;
+    transform: scaleX(2);
+  }
+
+  hr.visible {
+    display: block;
+  }
+
+  /* li {
+    margin-top: 6px;
+    padding-bottom: 6px;
+    border-bottom: solid 4px transparent;
+  } */
+
+  .visible {
+    border-bottom-color: #fff;
   }
 
   h2 {
